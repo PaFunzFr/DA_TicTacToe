b65ac0bb4797bb42f03a6920a148b8592fc06cb3
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-01-30 15:41:19 +0100

    added disableAllCells function

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let inputFields = [null, null, null, null, null, null, null, null, null];
+let currentPlayer = "cross";
+const board = document.getElementById("board");
 const winPatterns = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
     [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
     [0, 4, 8], [2, 4, 6]             // diagonal
 ];
-let currentPlayer = "cross";
-let setSymbol = value === "cross" ? "X" : "O"
-const board = document.getElementById("board");
 
 function initRender() {
     board.innerHTML = "";
     inputFields.forEach((value, index) => {
         let cell = createCell(index);
+        let setSymbol = value === "cross" ? "X" : "O";
         cell.innerHTML = value ? setSymbol : "";
-        disableIfContentInCell(value);
+        disableIfContentInCell(cell, value);
         cell.addEventListener("click", handleClick);
         board.appendChild(cell);
     });
@@ -26,9 +26,9 @@ function createCell(index) {
     return cell;
 }
 
-function disableIfContentInCell(value) {
+function disableIfContentInCell(cell, value) {
     if (value) {
-        cell.classList.add("taken"); // if already clicked disable
+        cell.classList.add("taken");
     };
 }
 
@@ -39,22 +39,28 @@ function handleClick(event) {
     let nextPlayer = currentPlayer === "cross" ? "circle" : "cross";
     currentPlayer = nextPlayer;
     initRender();
-    checkWinner();
+    checkForEnd();
 }
 
-function checkWinner() {
+function checkForEnd() {
     for (const pattern of winPatterns) {
         const [a, b, c] = pattern;
         if (inputFields[a] && inputFields[a] === inputFields[b] && inputFields[a] === inputFields[c]) {
             document.getElementById("winner").innerText = `${inputFields[a] === "cross" ? "X" : "O"} gewinnt!`;
-            return true;
+            disableAllCells();
         }
     }
     if (!inputFields.includes(null)) {
         document.getElementById("winner").innerText = "Unentschieden!";
-        return true;
     }
-    return false;
+}
+
+function disableAllCells() {
+    let allCells = document.querySelectorAll(".cell");
+    allCells.forEach((cell) => {
+        cell.classList.add("taken");
+        cell.removeEventListener("click", handleClick);
+    });
 }
 
 function resetGame() {
