689c6bc41bd389a9d14183fc6751845501b1b27b
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-01-30 15:14:32 +0100

    refactoring and adjustments to code

--- script.js
+++ script.js
@@ -5,26 +5,39 @@ const winPatterns = [
     [0, 4, 8], [2, 4, 6]             // diagonal
 ];
 let currentPlayer = "cross";
+let setSymbol = value === "cross" ? "X" : "O"
 const board = document.getElementById("board");
 
 function initRender() {
     board.innerHTML = "";
     inputFields.forEach((value, index) => {
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.dataset.index = index;
-        cell.innerHTML = value ? (value === "cross" ? "X" : "O") : "";
-        if (value) cell.classList.add("taken"); // if already clicked disable
+        let cell = createCell(index);
+        cell.innerHTML = value ? setSymbol : "";
+        disableIfContentInCell(value);
         cell.addEventListener("click", handleClick);
         board.appendChild(cell);
     });
 }
 
+function createCell(index) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.dataset.index = index;
+    return cell;
+}
+
+function disableIfContentInCell(value) {
+    if (value) {
+        cell.classList.add("taken"); // if already clicked disable
+    };
+}
+
 function handleClick(event) {
     const index = event.target.dataset.index;
-    if (inputFields[index] || checkWinner()) return;
+    if (inputFields[index]) return;
     inputFields[index] = currentPlayer;
-    currentPlayer = currentPlayer === "cross" ? "circle" : "cross";
+    let nextPlayer = currentPlayer === "cross" ? "circle" : "cross";
+    currentPlayer = nextPlayer;
     initRender();
     checkWinner();
 }
